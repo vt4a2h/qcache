942da9176e1b35cb201e4810180a95aa6ee5ea25
vt4a2h <vt4a2h@gmail.com>
2017-07-15 20:25:50 +0300

    Implemented methods for putting, making, and taking cache values.

--- Cache.hpp
+++ Cache.hpp
@@ -1,62 +1,92 @@
 #pragma once
 
 #include <cstddef>
+#include <cassert>
 #include <unordered_map>
 #include <memory>
+#include <functional>
 
 namespace sc {
 
-    template <class Key, class Value, class ValuePtr = std::shared_ptr<Value>>
-    class Cache
-    {
-    public:
-        Cache(std::size_t totalCost = 100)
-            : m_TotalCost(totalCost)
-        {}
+    namespace details {
 
-        ValuePtr putValue(Key const& key, Value * v, std::size_t cost = 1)
+        template <class Key, class Value, class ValuePtr, auto maker>
+        class Cache
         {
-            m_CurrentCost += cost;
-            // if (m_CurrentCost >= m_Costs) cleanup...
-            return (m_Values[key] = {cost, ValuePtr(v)});
-        }
+        public:
+            Cache(std::size_t totalCost = 100)
+                : m_TotalCost(totalCost)
+            {}
 
-        ValuePtr takeValue(Key const& key)
-        {
-            ValuePtr result;
-            auto it = m_Values.find(key);
-            if (it != std::end(m_Values))
+            ValuePtr putValue(Key const& key, Value * v, std::size_t cost = 1)
             {
-                result = it->value;
-                m_CurrentCost -= it->cost;
-
-                m_Values.erase(it);
+                return addValueImpl(key, [&v]{ return ValuePtr(v); }, cost);
             }
 
-            return result;
-        }
+            ValuePtr makeValue(Key const& key, std::size_t cost= 1)
+            {
+                return addValueImpl(key, maker, cost);
+            }
 
-        std::size_t totalCost() const
-        {
-            return m_TotalCost;
-        }
+            ValuePtr takeValue(Key const& key)
+            {
+                ValuePtr result;
+                auto it = m_Values.find(key);
+                if (it != std::end(m_Values))
+                {
+                    result = it->second.value;
+                    m_CurrentCost -= it->second.cost;
 
-        void setTotalCost(std::size_t totalCost)
-        {
-            m_TotalCost = totalCost;
-        }
+                    m_Values.erase(it);
+                }
 
-    private: // Types
-        struct ValueData
-        {
-            std::size_t cost;
-            ValuePtr value;
+                return result;
+            }
+
+            std::size_t totalCost() const
+            {
+                return m_TotalCost;
+            }
+
+            void setTotalCost(std::size_t totalCost)
+            {
+                m_TotalCost = totalCost;
+            }
+
+        private: // Types
+            struct ValueData
+            {
+                std::size_t cost;
+                ValuePtr value;
+            };
+
+        private: // Functions
+            template <class F>
+            ValuePtr addValueImpl(Key const& key, F func, std::size_t cost)
+            {
+                m_CurrentCost += cost;
+
+                // TODO:
+                // if (m_CurrentCost >= m_Costs) cleanup...
+
+                if (m_Values.find(key) != std::end(m_Values))
+                    m_Values.erase(key);
+
+                auto result = m_Values.emplace(key, ValueData{cost, func()});
+                assert(result.second);
+
+                return result.first->second.value;
+            }
+
+        private:
+            std::size_t m_TotalCost;
+            std::size_t m_CurrentCost;
+            std::unordered_map<Key, ValueData> m_Values;
         };
 
-    private:
-        std::size_t m_TotalCost;
-        std::size_t m_CurrentCost;
-        std::unordered_map<Key, ValueData> m_Values;
-    };
+    } // namespace details
 
-} // sc
+    template<class Key, class Value>
+    using Cache = details::Cache<Key, Value, std::shared_ptr<Value>, &std::make_shared<Value>>;
+
+} // namespace sc
--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
+#include <iostream>
+
 #include "Cache.hpp"
 
 int main()
 {
     sc::Cache<int, double> k;
+    auto v = k.putValue(3, new double(10));
+    std::cout << "v: " << *v << std::endl;
+    *v = 11;
+    std::cout << "v: " << *v << std::endl;
+
+    auto v1 = k.makeValue(4);
+    *v1 = 12;
+    std::cout << "v1: " << *v1 << std::endl;
+
+    auto v2 = k.takeValue(4);
+    std::cout << "v3: " << *v2 << std::endl;
+
 
     return 0;
 }
